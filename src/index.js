859a773619244b5ae12ec869323c7d4748bfb496
Dmitriy Metelya <dmitriymetelya@mail.ru>
2021-02-15 18:29:10 +0700

    Add genDiff for flat JSON
    Difference generator first version with library-type functionality.
    Not tested. Utility-type is yet to be developed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-const genDiff = () => {};
+import { readFileSync } from 'fs';
+
+const stringify = (diffs) => diffs.join('\n');
+
+const genDiff = (filepath1, filepath2) => {
+  const file1Content = readFileSync(filepath1, 'utf-8');
+  const file1Object = JSON.parse(file1Content);
+  const file2Content = readFileSync(filepath2, 'utf-8');
+  const file2Object = JSON.parse(file2Content);
+  const leftMergedObject = { ...file2Object, ...file1Object };
+  const leftMergedEntries = Object.entries(leftMergedObject);
+  const leftMergedEntriesSorted = _.sortBy(leftMergedEntries, ([key]) => key);
+  const diffs = leftMergedEntriesSorted.reduce((acc, [key, value]) => {
+    const newAcc = [...acc];
+
+    if (file1Object[key]) {
+      if (file2Object[key]) {
+        if (value === file2Object[key]) {
+          newAcc.push(`  ${key}: ${value}`);
+        } else {
+          newAcc.push(`- ${key}: ${value}`);
+          newAcc.push(`+ ${key}: ${file2Object[key]}`);
+        }
+      } else {
+        newAcc.push(`- ${key}: ${value}`);
+      }
+    } else {
+      newAcc.push(`+ ${key}: ${file2Object[key]}`);
+    }
+
+    return newAcc;
+  }, []);
+
+  return stringify(diffs);
+};
 
 export default genDiff;
