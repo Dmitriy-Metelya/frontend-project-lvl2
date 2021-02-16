a294a710c0abc9520825f0581cd7013fb869d665
Vladislav Prosvirnin <prosvirnin.vlad@gmail.com>
2021-02-16 14:28:03 +0300

    Fix the Jest test suite running.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,7 @@
+import { resolve } from 'path';
 import genDiff from '../src/index.js';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const flat3ToFlat4Diff =
   '  author: Dmitriy Metelya\n- description: my first project\n- license: ISC\n  main: bin/brain-games.js\n+ model: first\n  name: @hexlet/code\n  type: module\n- version: 5.0.1\n+ version: 0.4.0';
 const flat4ToFlat5Diff =
@@ -16,12 +13,12 @@ const flat4ToSelfDiff =
   '  author: Dmitriy Metelya\n  main: bin/brain-games.js\n  model: first\n  name: @hexlet/code\n  type: module\n  version: 0.4.0';
 
 test('common test', () => {
-  expect(genDiff(getFixturePath('flat3.json', 'flat4.json'))).toEqual(flat3ToFlat4Diff);
-  expect(genDiff('flat4.json', 'flat5.json')).toEqual(flat4ToFlat5Diff);
-  expect(genDiff('flat3.json', 'empty.json')).toEqual(flat3ToEmptyDiff);
+  expect(genDiff(getFixturePath('flat3.json'), getFixturePath('flat4.json'))).toEqual(flat3ToFlat4Diff);
+  expect(genDiff(getFixturePath('flat4.json'), getFixturePath('flat5.json'))).toEqual(flat4ToFlat5Diff);
+  expect(genDiff(getFixturePath('flat3.json'), getFixturePath('empty.json'))).toEqual(flat3ToEmptyDiff);
 });
 
 test('extremes test', () => {
-  expect(genDiff('empty.json', 'empty.json')).toEqual(emptyToSelfDiff);
-  expect(genDiff('flat4.json', 'flat4.json')).toEqual(flat4ToSelfDiff);
+  expect(genDiff(getFixturePath('empty.json'), getFixturePath('empty.json'))).toEqual(emptyToSelfDiff);
+  expect(genDiff(getFixturePath('flat4.json'), getFixturePath('flat4.json'))).toEqual(flat4ToSelfDiff);
 });
