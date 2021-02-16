e9f414369c6ad8ac7b7868000004f697d5e8653b
Dmitriy Metelya <dmitriymetelya@mail.ru>
2021-02-16 11:41:51 +0700

    Use the package as an utility
    Utility functionality finished. Run it in command line. Watch help
    (gendiff -h) for the details.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Dmitriy-Metelya/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Dmitriy-Metelya/frontend-project-lvl2/actions)
+
+Demo video of basic features and flat JSON files comparing https://asciinema.org/a/OvrQoYu1497oEwxKTxbQposDP
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,21 +3,29 @@
 import { program } from 'commander';
 import { readFileSync } from 'fs';
 import showHelp from '../src/help.js';
+import genDiff from '../src/index.js';
 
 const packageJson = readFileSync('package.json');
 const version = JSON.parse(packageJson);
 
 program
+  .version(version.version)
   .option('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .version(version.version)
   .parse();
 
 const options = program.opts();
 
 if (options.help) {
   showHelp();
-}
-if (options.format) {
-  // do something with options.format
+} else if (options.format) {
+  console.log(options.format);
+} else {
+  program
+    .arguments('<file1> <file2>')
+    .description('compare files')
+    .action((file1, file2) => {
+      console.log(genDiff(file1, file2));
+    })
+    .parse();
 }
