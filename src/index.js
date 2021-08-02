1e59be5a15028481843bc87c6d6c194a6d3fb0d1
Dmitriy Metelya <dmitriymetelya@mail.ru>
2021-08-02 13:55:08 +0700

    Separate representer and formatter
    Separate file structure representing and output formatting in the logic.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,15 @@
-import { has, sortBy } from 'lodash';
 import parseFile from './diff/parsers.js';
-
-const stringify = (diffs) => diffs.join('\n');
+import representDiff from './diff/representer.js';
+import formatDiff from './diff/formatter.js';
 
 const genDiff = (filepath1, filepath2) => {
   const file1Object = parseFile(filepath1);
   const file2Object = parseFile(filepath2);
-  const leftMergedObject = { ...file2Object, ...file1Object };
-  const leftMergedEntries = Object.entries(leftMergedObject);
-  const leftMergedEntriesSorted = sortBy(leftMergedEntries, ([key]) => key);
-  const diffs = leftMergedEntriesSorted.reduce((acc, [key, value]) => {
-    const newAcc = [...acc];
 
-    if (has(file1Object, key)) {
-      if (has(file2Object, key)) {
-        if (value === file2Object[key]) {
-          newAcc.push(`  ${key}: ${value}`);
-        } else {
-          newAcc.push(`- ${key}: ${value}`);
-          newAcc.push(`+ ${key}: ${file2Object[key]}`);
-        }
-      } else {
-        newAcc.push(`- ${key}: ${value}`);
-      }
-    } else {
-      newAcc.push(`+ ${key}: ${file2Object[key]}`);
-    }
+  const diffRepresenter = representDiff(file1Object, file2Object);
+  const diffView = formatDiff(diffRepresenter);
 
-    return newAcc;
-  }, []);
-
-  return stringify(diffs);
+  return diffView;
 };
 
 export default genDiff;
